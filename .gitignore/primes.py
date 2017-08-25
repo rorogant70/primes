51c404b57bd8e94196cdb5b1581b87fb85f2e420
rorogant70 <ronagurl19@gmail.com>
2017-08-25 10:36:25 -0400

    Update and rename .gitignore to primes.py

---
+++ .gitignore/primes.py
@@ -0,0 +1,14 @@
+
+number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+x = input("insert random number from 1-20.")
+y = input ('insert other random number from 1-20.')
+
+def prime():
+    if x//y == 1:
+        print ('prime')
+
+    elif x//y >= 2:
+        print ('not prime')
+
+for i 
